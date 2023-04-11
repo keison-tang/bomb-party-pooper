8c87f804b0b8d14e740c92941be96de583e1293f
Keison <keison-tang@users.noreply.github.com>
2023-04-11 23:49:21 +1200

    add self turn indicator to msg

--- scripts/content.js
+++ scripts/content.js
@@ -7,11 +7,41 @@ function sendMessage(syllable) {
   const json = {
     action: 'SendSyllable',
     syllable: syllable,
+    isSelfTurn: isSelfTurn(),
   };
 
   console.log('Sending message from content.js ' + JSON.stringify(json));
 
   chrome.runtime.sendMessage(json);
+}
+
+/**
+ * Indicates whether or not it is the player's turn.
+ *
+ * If selfTurn or round (parent element of selfTurn) has the hidden attribute,
+ * then it's not the player's turn
+ *
+ * @returns boolean
+ */
+function isSelfTurn() {
+  const selfTurnElement = document.getElementsByClassName('selfTurn')[0];
+
+  if (!selfTurnElement) {
+    return false;
+  }
+
+  const selfTurnHiddenAttribute =
+    selfTurnElement.attributes.getNamedItem('hidden');
+
+  const parentElement = selfTurnElement.parentElement;
+  const parentElementHiddenAttribute =
+    parentElement.attributes.getNamedItem('hidden');
+
+  if (selfTurnHiddenAttribute || parentElementHiddenAttribute) {
+    return false;
+  }
+
+  return true;
 }
 
 function findSyllable() {
